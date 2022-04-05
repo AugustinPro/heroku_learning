5bd1f0fd052ea09fe33231b456db5e0346c8557c
Augustin <apetre.pro@gmail.com>
2022-04-05 12:46:06 +0200

    init

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,14 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+require('bootstrap');
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
